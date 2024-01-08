8bac98ce67c4f80fbb1e7b27546e2b39d1ef85fa
Ripca <ricardoarrecisrivera@gmail.com>
2024-01-08 15:02:38 -0600

    Se agregan todos los archivos del programa

---
+++ CalculadoraIMC/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace CalculadoraIMC
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.txtEstatura = new System.Windows.Forms.TextBox();
+            this.ptbImage = new System.Windows.Forms.PictureBox();
+            this.dgvIMC = new System.Windows.Forms.DataGridView();
+            this.lblResultado = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvIMC)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(195, 169);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(171, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "¡Calcula tu IMC!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(98, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Peso en kg:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(67, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Estatura en metros:";
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(182, 90);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(100, 23);
+            this.txtPeso.TabIndex = 4;
+            // 
+            // txtEstatura
+            // 
+            this.txtEstatura.Location = new System.Drawing.Point(182, 122);
+            this.txtEstatura.Name = "txtEstatura";
+            this.txtEstatura.Size = new System.Drawing.Size(100, 23);
+            this.txtEstatura.TabIndex = 5;
+            // 
+            // ptbImage
+            // 
+            this.ptbImage.Location = new System.Drawing.Point(337, 69);
+            this.ptbImage.Name = "ptbImage";
+            this.ptbImage.Size = new System.Drawing.Size(127, 113);
+            this.ptbImage.TabIndex = 6;
+            this.ptbImage.TabStop = false;
+            // 
+            // dgvIMC
+            // 
+            this.dgvIMC.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dgvIMC.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvIMC.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvIMC.GridColor = System.Drawing.SystemColors.ButtonFace;
+            this.dgvIMC.Location = new System.Drawing.Point(34, 246);
+            this.dgvIMC.Name = "dgvIMC";
+            this.dgvIMC.RowTemplate.Height = 25;
+            this.dgvIMC.Size = new System.Drawing.Size(465, 97);
+            this.dgvIMC.TabIndex = 7;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(157, 212);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 15);
+            this.lblResultado.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 392);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.dgvIMC);
+            this.Controls.Add(this.ptbImage);
+            this.Controls.Add(this.txtEstatura);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ptbImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvIMC)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txtPeso;
+        private TextBox txtEstatura;
+        private PictureBox ptbImage;
+        private DataGridView dgvIMC;
+        private Label lblResultado;
+    }
+}
---
+++ CalculadoraIMC/Form1.cs
@@ -0,0 +1,72 @@
+using System.Data;
+
+namespace CalculadoraIMC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                double Estatura, Peso, IMC;
+                string rangoDePeso = "", imcRango = "", NivelDePeso = "";
+
+                Estatura = double.Parse(txtEstatura.Text);
+                Peso = double.Parse(txtPeso.Text);
+
+                IMC = Math.Round(Peso / (Math.Pow(Estatura, 2)), 2);
+
+                lblResultado.Text = "Tu imc es: " + IMC;
+
+                if (IMC < 18.5)
+                {
+                    rangoDePeso = "124 lb o menos";
+                    imcRango = "Por debajo de 18.5";
+                    NivelDePeso = "Bajo peso";
+                }
+                else if (IMC >= 18.5 && IMC <= 24.9)
+                {
+                    rangoDePeso = "125 a 168 lb";
+                    imcRango = "18.5 a 24.9 ";
+                    NivelDePeso = "Normal";
+                }
+                else if (IMC >= 25 && IMC <= 29.9)
+                {
+                    rangoDePeso = "169 lb a 202 lb";
+                    imcRango = "25.0 a 29.9";
+                    NivelDePeso = "Sobrepaso";
+                }
+                else if (IMC >= 30)
+                {
+                    rangoDePeso = "203 lb o mas";
+                    imcRango = "30 o más";
+                    NivelDePeso = "Obesidad";
+                }
+
+                //Creacion de tabla para luego colocarse al DataGridView
+
+
+                DataTable tabla = new DataTable();
+                tabla.Columns.Add("Estatura", typeof(string));
+                tabla.Columns.Add("Rango de peso", typeof(string));
+                tabla.Columns.Add("IMC", typeof(string));
+                tabla.Columns.Add("Nivel de peso", typeof(string));
+
+                tabla.Rows.Add(Estatura + " m", rangoDePeso, imcRango, NivelDePeso);
+                dgvIMC.DataSource = tabla;
+            }
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Image image = Image.FromFile("imc.png");
+            ptbImage.Image = image;
+            ptbImage.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+    }
+}
